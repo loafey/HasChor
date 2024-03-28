a1d91f55a986781e0866e288c46eea87339a9265
Rewbert <krookr@chalmers.se>
2024-03-28 17:13:46 +0100

    better output

--- examples/tricky/Main.hs
+++ examples/tricky/Main.hs
@@ -10,7 +10,7 @@ import Choreography.Choreo
 import Choreography.Location
 import Choreography.Network.Http
 import Data.Proxy
-import GHC.TypeLits (KnownSymbol)
+import GHC.TypeLits (KnownSymbol, symbolVal)
 import System.Environment
 
 $(compileFor 0         [ ("worker1", ("localhost", 4000))
@@ -22,7 +22,9 @@ $(compileFor 0         [ ("worker1", ("localhost", 4000))
 alternate :: (KnownSymbol a, KnownSymbol b) => Proxy a -> Proxy b -> Int @ a -> Int @ b -> Choreo IO ()
 alternate a b ai bi = do
     aatb <- (a,ai) ~> b
-    locally b $ \unwrap -> print ("received from a at b: " <> (show $ unwrap aatb))
+    let s = symbolVal a
+        ss = symbolVal b
+    locally b $ \unwrap -> print ("received from " <> s <> " at " <> ss <> ": " <> (show $ unwrap aatb))
     ai' <- locally a $ \unwrap -> return (unwrap ai + 1)
     alternate b a bi ai'
 
