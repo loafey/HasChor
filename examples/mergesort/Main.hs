92250e9d3d991df18426a6051aaa36f5785d580f
Rewbert <krookr@chalmers.se>
2024-03-29 11:55:46 +0100

    mergesort works, if you uncomment and comment in the right set of rules between compilations

--- examples/mergesort/Main.hs
+++ examples/mergesort/Main.hs
@@ -21,12 +21,219 @@ divide xs = splitAt lhx xs
   where
     lhx = length xs `div` 2
 
-$(compileFor 0         [ ("primary", ("localhost", 3000))
-                       , ("worker1", ("localhost", 4000))
-                       , ("worker2", ("localhost", 5000))
-                       ])
+-- $(compileFor 2         [ ("primary", ("localhost", 3000))
+--                        , ("worker1", ("localhost", 4000))
+--                        , ("worker2", ("localhost", 5000))
+--                        ])
 
-{-# INLINE sort #-}
+{-# RULES "CHOREORULES locallyandsend"
+              forall l (m::(forall a. At a l -> a) -> m a) l'. (l, m) ~~> l'
+                = do x <- l `locally` m
+                     (l, x) ~> l' #-}
+{-# RULES "CHOREORULES condprime"
+              forall l (m::(forall a. At a l -> a) -> m a) l'. (l, m) `cond'` l'
+                = do x <- l `locally` m
+                     (l, x) `cond` l' #-}
+
+primary :: Proxy "primary"
+primary = Proxy
+
+worker1 :: Proxy "worker1"
+worker1 = Proxy
+
+worker2 :: Proxy "worker2"
+worker2 = Proxy
+{-# RULES "CHOREORULES locallyprimary"
+              forall (f::(forall a. At a "primary" -> a) -> m a) (primary :: Proxy "primary"). primary
+                                                                    `locally` f
+                = wrap <$> run (f unwrap) #-}
+{-# RULES "CHOREORULES locallyworker1"
+              forall (f::(forall a. At a "worker1" -> a) -> m a) (worker1 :: Proxy "worker1"). worker1
+                                                                    `locally` f
+                = return mkEmpty #-}
+{-# RULES "CHOREORULES locallyworker2"
+              forall (f::(forall a. At a "worker2" -> a) -> m a) (worker2 :: Proxy "worker2"). worker2
+                                                                    `locally` f
+                = return mkEmpty #-}
+{-# RULES "CHOREORULES sendfromprimary"
+              forall v recipient (primary :: Proxy "primary"). (primary, v) ~> recipient
+                = (send (unwrap v) (toLocTm recipient)) >> return mkEmpty #-}
+{-# RULES "CHOREORULES sendtoprimary"
+              forall from v (primary :: Proxy "primary"). (from, v) ~> primary
+                = wrap <$> recv (toLocTm from) #-}
+{-# RULES "CHOREORULES sendworker1worker2"
+              forall v (worker1 :: Proxy "worker1") (worker2 :: Proxy "worker2"). (worker1, v) ~> worker2
+                = return mkEmpty #-}
+{-# RULES "CHOREORULES sendworker2worker1"
+              forall v (worker1 :: Proxy "worker1") (worker2 :: Proxy "worker2"). (worker2, v) ~> worker1
+                = return mkEmpty #-}
+{-# RULES "CHOREORULES condprimary"
+              forall v c (primary :: Proxy "primary"). (primary, v) `cond` c
+                = (broadcast (unwrap v)) >> c (unwrap v) #-}
+{-# RULES "CHOREORULES condworker1"
+              forall v c (worker1 :: Proxy "worker1"). (worker1, v) `cond` c
+                = (recv (toLocTm worker1)) >>= \ x -> c x #-}
+{-# RULES "CHOREORULES condworker2"
+              forall v c (worker2 :: Proxy "worker2"). (worker2, v) `cond` c
+                = (recv (toLocTm worker2)) >>= \ x -> c x #-}
+run'_a3Z8 choreo_a3Z9
+  = (runChoreography
+       (mkHttpConfig
+          [("primary", ("localhost", 3000)),
+           ("worker1", ("localhost", 4000)),
+           ("worker2", ("localhost", 5000))])
+       choreo_a3Z9 "primary"
+       >> return ())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- {-# RULES "CHOREORULES locallyandsend"
+--               forall l (m::(forall a. At a l -> a) -> m a) l'. (l, m) ~~> l'
+--                 = do x <- l `locally` m
+--                      (l, x) ~> l' #-}
+-- {-# RULES "CHOREORULES condprime"
+--               forall l (m::(forall a. At a l -> a) -> m a) l'. (l, m) `cond'` l'
+--                 = do x <- l `locally` m
+--                      (l, x) `cond` l' #-}
+
+-- primary :: Proxy "primary"
+-- primary = Proxy
+
+-- worker1 :: Proxy "worker1"
+-- worker1 = Proxy
+
+-- worker2 :: Proxy "worker2"
+-- worker2 = Proxy
+-- {-# RULES "CHOREORULES locallyworker1"
+--               forall (f::(forall a. At a "worker1" -> a) -> m a) (worker1 :: Proxy "worker1"). worker1
+--                                                                     `locally` f
+--                 = wrap <$> run (f unwrap) #-}
+-- {-# RULES "CHOREORULES locallyprimary"
+--               forall (f::(forall a. At a "primary" -> a) -> m a) (primary :: Proxy "primary"). primary
+--                                                                     `locally` f
+--                 = return mkEmpty #-}
+-- {-# RULES "CHOREORULES locallyworker2"
+--               forall (f::(forall a. At a "worker2" -> a) -> m a) (worker2 :: Proxy "worker2"). worker2
+--                                                                     `locally` f
+--                 = return mkEmpty #-}
+-- {-# RULES "CHOREORULES sendfromworker1"
+--               forall v recipient (worker1 :: Proxy "worker1"). (worker1, v) ~> recipient
+--                 = (send (unwrap v) (toLocTm recipient)) >> return mkEmpty #-}
+-- {-# RULES "CHOREORULES sendtoworker1"
+--               forall from v (worker1 :: Proxy "worker1"). (from, v) ~> worker1
+--                 = wrap <$> recv (toLocTm from) #-}
+-- {-# RULES "CHOREORULES sendprimaryworker2"
+--               forall v (primary :: Proxy "primary") (worker2 :: Proxy "worker2"). (primary, v) ~> worker2
+--                 = return mkEmpty #-}
+-- {-# RULES "CHOREORULES sendworker2primary"
+--               forall v (primary :: Proxy "primary") (worker2 :: Proxy "worker2"). (worker2, v) ~> primary
+--                 = return mkEmpty #-}
+-- {-# RULES "CHOREORULES condworker1"
+--               forall v c (worker1 :: Proxy "worker1"). (worker1, v) `cond` c
+--                 = (broadcast (unwrap v)) >> c (unwrap v) #-}
+-- {-# RULES "CHOREORULES condprimary"
+--               forall v c (primary :: Proxy "primary"). (primary, v) `cond` c
+--                 = (recv (toLocTm primary)) >>= \ x -> c x #-}
+-- {-# RULES "CHOREORULES condworker2"
+--               forall v c (worker2 :: Proxy "worker2"). (worker2, v) `cond` c
+--                 = (recv (toLocTm worker2)) >>= \ x -> c x #-}
+-- run'_a41m choreo_a41n
+--   = (runChoreography
+--        (mkHttpConfig
+--           [("primary", ("localhost", 3000)),
+--            ("worker1", ("localhost", 4000)),
+--            ("worker2", ("localhost", 5000))])
+--        choreo_a41n "worker1"
+--        >> return ())
+
+
+
+
+
+
+
+
+
+
+
+-- {-# RULES "CHOREORULES locallyandsend"
+--               forall l (m::(forall a. At a l -> a) -> m a) l'. (l, m) ~~> l'
+--                 = do x <- l `locally` m
+--                      (l, x) ~> l' #-}
+-- {-# RULES "CHOREORULES condprime"
+--               forall l (m::(forall a. At a l -> a) -> m a) l'. (l, m) `cond'` l'
+--                 = do x <- l `locally` m
+--                      (l, x) `cond` l' #-}
+
+-- primary :: Proxy "primary"
+-- primary = Proxy
+
+-- worker1 :: Proxy "worker1"
+-- worker1 = Proxy
+
+-- worker2 :: Proxy "worker2"
+-- worker2 = Proxy
+-- {-# RULES "CHOREORULES locallyworker2"
+--               forall (f::(forall a. At a "worker2" -> a) -> m a) (worker2 :: Proxy "worker2"). worker2
+--                                                                     `locally` f
+--                 = wrap <$> run (f unwrap) #-}
+-- {-# RULES "CHOREORULES locallyprimary"
+--               forall (f::(forall a. At a "primary" -> a) -> m a) (primary :: Proxy "primary"). primary
+--                                                                     `locally` f
+--                 = return mkEmpty #-}
+-- {-# RULES "CHOREORULES locallyworker1"
+--               forall (f::(forall a. At a "worker1" -> a) -> m a) (worker1 :: Proxy "worker1"). worker1
+--                                                                     `locally` f
+--                 = return mkEmpty #-}
+-- {-# RULES "CHOREORULES sendfromworker2"
+--               forall v recipient (worker2 :: Proxy "worker2"). (worker2, v) ~> recipient
+--                 = (send (unwrap v) (toLocTm recipient)) >> return mkEmpty #-}
+-- {-# RULES "CHOREORULES sendtoworker2"
+--               forall from v (worker2 :: Proxy "worker2"). (from, v) ~> worker2
+--                 = wrap <$> recv (toLocTm from) #-}
+-- {-# RULES "CHOREORULES sendprimaryworker1"
+--               forall v (primary :: Proxy "primary") (worker1 :: Proxy "worker1"). (primary, v) ~> worker1
+--                 = return mkEmpty #-}
+-- {-# RULES "CHOREORULES sendworker1primary"
+--               forall v (primary :: Proxy "primary") (worker1 :: Proxy "worker1"). (worker1, v) ~> primary
+--                 = return mkEmpty #-}
+-- {-# RULES "CHOREORULES condworker2"
+--               forall v c (worker2 :: Proxy "worker2"). (worker2, v) `cond` c
+--                 = (broadcast (unwrap v)) >> c (unwrap v) #-}
+-- {-# RULES "CHOREORULES condprimary"
+--               forall v c (primary :: Proxy "primary"). (primary, v) `cond` c
+--                 = (recv (toLocTm primary)) >>= \ x -> c x #-}
+-- {-# RULES "CHOREORULES condworker1"
+--               forall v c (worker1 :: Proxy "worker1"). (worker1, v) `cond` c
+--                 = (recv (toLocTm worker1)) >>= \ x -> c x #-}
+-- run'_a41m choreo_a41n
+--   = (runChoreography
+--        (mkHttpConfig
+--           [("primary", ("localhost", 3000)),
+--            ("worker1", ("localhost", 4000)),
+--            ("worker2", ("localhost", 5000))])
+--        choreo_a41n "worker2"
+--        >> return ())
+
+
+{-# SPECIALISE forall . sort primary worker1 worker2 #-}
+{-# SPECIALISE forall . sort primary worker2 worker1 #-}
+{-# SPECIALISE forall . sort worker2 primary worker1 #-}
+{-# SPECIALISE forall . sort worker1 primary worker2 #-}
+{-# SPECIALISE forall . sort primary worker1 worker2 #-}
+{-# SPECIALISE forall . sort primary worker2 worker1 #-}
 sort ::
   KnownSymbol a =>
   Proxy a ->
@@ -52,7 +259,12 @@ sort a b c lst = do
     False -> do
       return lst
 
-{-# INLINE merge #-}
+{-# SPECIALISE forall . merge primary worker1 worker2 #-}
+{-# SPECIALISE forall . merge primary worker2 worker1 #-}
+{-# SPECIALISE forall . merge worker2 primary worker1 #-}
+{-# SPECIALISE forall . merge worker1 primary worker2 #-}
+{-# SPECIALISE forall . merge primary worker1 worker2 #-}
+{-# SPECIALISE forall . merge primary worker2 worker1 #-}
 merge ::
   KnownSymbol a =>
   Proxy a ->
@@ -103,7 +315,7 @@ mainChoreo = do
   return ()
 
 main :: IO ()
-main = run' mainChoreo
+main = run'_a3Z8 mainChoreo
 
 -- main :: IO ()
 -- main = do
@@ -120,3 +332,13 @@ main = run' mainChoreo
 --           ("worker1", ("localhost", 4000)),
 --           ("worker2", ("localhost", 5000))
 --         ]
+
+
+-- primary old = 160 rules
+-- primary new = 161 rules
+
+-- worker1 old = 160 rules
+-- worker1 new = 161 rules
+
+-- worker2 old = 160 rules
+-- worker2 new = 161 rules
--- examples/tricky/Main.hs
+++ examples/tricky/Main.hs
@@ -13,9 +13,51 @@ import Data.Proxy
 import GHC.TypeLits (KnownSymbol, symbolVal)
 import System.Environment
 
-$(compileFor 0         [ ("worker1", ("localhost", 4000))
-                       , ("worker2", ("localhost", 5000))
-                       ])
+{-# RULES "CHOREORULES locallyandsend"
+              forall l (m::(forall a. At a l -> a) -> m a) l'. (l, m) ~~> l'
+                = do x <- l `locally` m
+                     (l, x) ~> l' #-}
+{-# RULES "CHOREORULES condprime"
+              forall l (m::(forall a. At a l -> a) -> m a) l'. (l, m) `cond'` l'
+                = do x <- l `locally` m
+                     (l, x) `cond` l' #-}
+
+worker1 :: Proxy "worker1"
+worker1 = Proxy
+
+worker2 :: Proxy "worker2"
+worker2 = Proxy
+{-# RULES "CHOREORULES locallyworker2"
+              forall (f::(forall a. At a "worker2" -> a) -> m a) (worker :: Proxy "worker2"). worker
+                                                                    `locally` f
+                = wrap <$> run (f unwrap) #-}
+{-# RULES "CHOREORULES locallyworker1"
+              forall (f::(forall a. At a "worker1" -> a) -> m a) (worker :: Proxy "worker1"). worker
+                                                                    `locally` f
+                = return mkEmpty #-}
+{-# RULES "CHOREORULES sendfromworker2"
+              forall v recipient (worker :: Proxy "worker2"). (worker, v) ~> recipient
+                = (send (unwrap v) (toLocTm recipient)) >> return mkEmpty #-}
+{-# RULES "CHOREORULES sendtoworker2"
+              forall from v (worker :: Proxy "worker2"). (from, v) ~> worker
+                = wrap <$> recv (toLocTm from) #-}
+{-# RULES "CHOREORULES condworker2"
+              forall v c (worker :: Proxy "worker2"). (worker, v) `cond` c
+                = (broadcast (unwrap v)) >> c (unwrap v) #-}
+{-# RULES "CHOREORULES condworker1"
+              forall v c (worker :: Proxy "worker1"). (worker, v) `cond` c
+                = (recv (toLocTm worker)) >>= \ x -> c x #-}
+run'_a3z2 choreo_a3z3
+  = (runChoreography
+       (mkHttpConfig
+          [("worker1", ("localhost", 4000)),
+           ("worker2", ("localhost", 5000))])
+       choreo_a3z3 "worker2"
+       >> return ())
+
+-- $(compileFor 1         [ ("worker1", ("localhost", 4000))
+--                        , ("worker2", ("localhost", 5000))
+--                        ])
 
 {-# SPECIALISE forall . alternate worker1 worker2 #-}
 {-# SPECIALISE forall . alternate worker2 worker1 #-}
@@ -29,4 +71,6 @@ alternate a b ai bi = do
     alternate b a bi ai'
 
 main :: IO ()
-main = run' (alternate worker1 worker2 (wrap 0) (wrap 0))
+main = run'_a3z2 (alternate worker1 worker2 (wrap 0) (wrap 0))
+
+-- 6 rules
